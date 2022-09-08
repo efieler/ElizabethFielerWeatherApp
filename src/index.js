3caa820e0227f23ba232894e177e88beef6a912a
efieler <112910565+efieler@users.noreply.github.com>
2022-09-08 17:32:31 -0500

    Made date and time work correctly with search
    Now, the date and time are updated with each new search and not just when the page is refreshed/ newly opened

--- src/index.js
+++ src/index.js
@@ -52,6 +52,42 @@ function showCity(event) {
     let humidity = document.querySelector("#humidity");
     let currentHumidity = response.data.main.humidity;
     humidity.innerHTML = `Humidity: ${currentHumidity}%`;
+    let now = new Date();
+    let h4 = document.querySelector("h4");
+    let date = now.getDate();
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[now.getDay()];
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    if (minutes <= 9) {
+      minutes = `0${minutes}`;
+    }
+    let year = now.getFullYear();
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let month = months[now.getMonth()];
+    h4.innerHTML = `${month} ${date}, ${year} </br> ${day}, ${hours}:${minutes}`;
   }
   let city = `${searchInput.value}`;
   let apiKey = "4fa0a71d5203b32794359d1f5d59a57e";
