5e0fec005beb56c1a248de67572b55a27a84e6fb
efieler <112910565+efieler@users.noreply.github.com>
2022-09-17 12:26:02 -0500

    removed unit conversion and repeated js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,3 @@
-let now = new Date();
-let h4 = document.querySelector("h4");
-let date = now.getDate();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let hours = now.getHours();
-let minutes = now.getMinutes();
-if (minutes <= 9) {
-  minutes = `0${minutes}`;
-}
-let year = now.getFullYear();
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
-h4.innerHTML = `${month} ${date}, ${year} </br> ${day}, ${hours}:${minutes}`;
-
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
@@ -69,10 +32,10 @@ function displayForecast(response) {
         <li class="list-group-item fivedayemoji">🌤</li>
         <li class="list-group-item hightemp">${Math.round(
           forecastDay.temp.max
-        )}°C</li>
+        )}°F</li>
         <li class="list-group-item lowtemp">${Math.round(
           forecastDay.temp.min
-        )}°C</li>
+        )}°F</li>
       </ul>
      </div>
    </div>`;
@@ -84,9 +47,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "3bc520cc14bbdedfd7e45158f2ef0439";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -108,7 +70,7 @@ function displayCurrentWeather(response) {
   descriptions.innerHTML = `${currentDescription}`;
   ctemp.innerHTML = Math.round(response.data.main.temp);
   celsiusTemperature = response.data.main.temp;
-  tempUnit.innerHTML = `°C`;
+  tempUnit.innerHTML = `°F`;
   getForecast(response.data.coord);
   let currentWeatherEmoji = response.data.weather[0].icon;
   if (currentWeatherEmoji === "01n") {
@@ -234,7 +196,7 @@ function displayCurrentWeather(response) {
 
 function search(city) {
   let apiKey = "3bc520cc14bbdedfd7e45158f2ef0439";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayCurrentWeather);
 }
 
@@ -244,31 +206,7 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function showFahrenTemp(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#ctemp");
-  let ftemp = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(ftemp);
-  let tempUnit = document.querySelector("#tempUnit");
-  tempUnit.innerHTML = `°F`;
-}
-function showCelTemp(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#ctemp");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  let tempUnit = document.querySelector("#tempUnit");
-  tempUnit.innerHTML = `°C`;
-}
-
-let celsiusTemperature = null;
-
 let form = document.querySelector("#city-search");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahren-link");
-fahrenheitLink.addEventListener("click", showFahrenTemp);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", showCelTemp);
 
 search("Dallas");
